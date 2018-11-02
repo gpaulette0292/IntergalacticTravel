ede08b99fd33a313619fb31d761d1c74ca2d51e9
Grace Paulette <gpaulette0292@jh-mba158-15.d214.org>
2018-11-02 11:38:33 -0500

    added UIImage to storyboard, connected it as IBOutlet

---
+++ StarViewController.swift
@@ -0,0 +1,30 @@
+//
+//  StarViewController.swift
+//  IntergalacticTravel
+//
+//  Created by Grace Paulette on 11/2/18.
+//  Copyright © 2018 John Hersey HIgh School. All rights reserved.
+//
+
+import UIKit
+
+class StarViewController: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
